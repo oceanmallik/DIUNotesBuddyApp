e7cc7fc6611ab837d436e2035eff67b97011b1cb
Ocean Mallik <oceanmallik@oceanmallik.com>
2026-06-02 00:52:07 +0600

    Add main app component with background image and update TypeScript config

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,42 @@
+import { ImageBackground } from 'expo-image'
+import { StyleSheet, Text, View } from 'react-native'
+
+import backgroundImage from "@/assets/images/background.png"
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={backgroundImage}
+        resizeMode="cover"
+        style={styles.Image}
+      >
+        <Text style={styles.text}>DIU Notes Buddy</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  Image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    fontFamily: 'Times New Roman',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+})
